7bd88e61cdba6508cd89c95865f88155b4a20f00
LouisDumont <33422071+LouisDumont@users.noreply.github.com>
2018-03-03 18:40:32 +0100

    calcul du gradient ajouté dans oracle

--- oracle.py
+++ oracle.py
@@ -5,14 +5,17 @@ from numpy import dot
 
 from structure_r import *
 
-def OraclePG(qc):
+def OraclePG(qc,indice):
     Q0 = np.zeros(n)
     Q0[:md] = dot(AdI, fd)
     q = Q0 + dot(B, qc) 
     # critère
     F = 1./3*dot(q, r*q*np.abs(q)) + dot(pr, dot(Ar, q))
     # TODO dérivée du critère par rapport à qc
-    G = 0 
+    G = np.zeros(n-md)
+    G = (1/3)*dot(np.transpose(B),r*q*abs(q))
+    G += (2/3)*dot(np.transpose(B),r*q*abs(q))
+    G += dot(dot(np.transpose(B),np.transpose(Ar)),pr)
     return F, G
 
 if __name__ == '__main__':
