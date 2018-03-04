e87105b987a2b52d60dafa5eb65ccd3facc24abd
LouisDumont <33422071+LouisDumont@users.noreply.github.com>
2018-03-04 13:02:13 +0100

    hydrauliqueD now working

--- hydrauliqueD.py
+++ hydrauliqueD.py
@@ -1,13 +1,25 @@
+#!/usr/bin/python
+
 from structure_r import *
+from math import *
 
 
 def HydrauliqueD(pd):
     # Pressions aux noeuds
-    p = [pr,pd]
+    p = np.zeros(m)
+    p[:mr] = pr
+    p[mr:m] = pd
     # Pertes de charge des arcs
-    z = - np.transpose(A) * p
+    z = dot(-np.transpose(A),p)
     # Debits des arcs
-    q = z / sqrt(r*abs(z))
+    q = z / sqrt(dot(r,abs(z)))
     # Flux aux noeuds
-    f = [ Ar*q , fd ]
+    f = np.zeros(m)
+    f[:mr] = dot(Ar,q)
+    f[mr:m]= fd
     return(q,z,f,p)
+    
+if __name__ == '__main__':
+    pd = np.array([2 for i in range(md)])
+    print(HydrauliqueD(pd)[2])
+    
