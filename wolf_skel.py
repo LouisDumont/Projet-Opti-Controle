dc244072c6611392f1387cba1b701e3346795987
LouisDumont <33422071+LouisDumont@users.noreply.github.com>
2018-03-09 10:09:27 +0100

    New version

--- wolf_skel.py
+++ wolf_skel.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python
+import numpy as np
+
+def wolf(alpha, x, D, oracle):
+    
+    # Coefficients de la recherche lineaire
+    omega1 = 0.1
+    omega2 = 0.9
+    alphamin = 0.0
+    alphamax = np.inf
+    ok = 0
+    dltx = 0.00000001
+    
+    return(alphan,ok)
