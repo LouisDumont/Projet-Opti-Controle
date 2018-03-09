ae74a79485fd0f1965d345e8b77941dc6026989e
LouisDumont <33422071+LouisDumont@users.noreply.github.com>
2018-03-09 10:46:48 +0100

    Wolf compilant

--- wolf_skel.py
+++ wolf_skel.py
@@ -6,38 +6,55 @@ from numpy import dot
 def wolf(alpha, x, D, oracle):
     
     # Coefficients de la recherche lineaire
-    omega1 = 0.1
-    omega2 = 0.9
-    alphamin = 0.0
-    alphamax = np.inf
+    omega_1 = 0.1
+    omega_2 = 0.9
+    alpha_min = 0.0
+    alpha_max = np.inf
     ok = 0
     dltx = 0.00000001
     
     # Algorithme de Fletcher-Lemarechal
     
     # Appel de l'oracle au point initial
-    [F,G] = oracle(x)
+    loss, gradient, heissien = oracle(x)
     # Initialisation de l'algorithme
-    alphan = alpha
+    alpha_n = alpha
     xn     = x
     # Boucle de calcul du pas
     # xn represente le point pour la valeur courante du pas,
     # xp represente le point pour la valeur precedente du pas.
     while (ok==0):
         xp = xn
-        xn = x + dot(alphan,D)
+        xn = x + alpha_n*D
         
-        # Calcul des conditions de Wolfe
-        # TO DO
-        
+        # Calcul des conditions de Wolf
+        loss_n, gradient_n, heissien_n = oracle(xp)
+        wolf_1 = (loss_n - loss <= omega_1 * alpha_n * dot(gradient,D))
+        wolf_2 = (dot(gradient_n,D) >= omega_2 * dot(gradient,D))
+        print(loss_n,gradient_n)
         #Test de la valeur de alphan :
         #- si les deux conditions de Wolfe sont verifiees,
         #   faire ok = 1 : on sort alors de la boucle while
         #- sinon, modifier la valeur de alphan : on reboucle.
-        # TO DO
+        if wolf_1:
+            if wolf_2:
+                ok = 1
+            else:
+                if alpha_max==np.inf: alpha_n = 2 * alpha_min
+                else: alpha_n = (1/2) * (alpha_min + alpha_max)
+        else:
+            alpha_max = alpha_n
+            alpha_n = (1/2) * (alpha_min + alpha_max)
         
+        loss, gradient = loss_n, gradient_n
+                
         # Test d'indistinguabilite
-        if (abs(xn-xp)<dltx):
+        if (np.linalg.norm(xn-xp)<dltx):
             ok = 2
     
-    return(alphan,ok)
+    return(alpha_n,ok)
+    
+if __name__=="__main__":
+    x = np.random.normal(size=n-md)
+    D = np.random.normal(size=n-md)
+    wolf(1,x,D,oracle)
