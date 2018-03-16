fe3cc29b7a8ca79eb83dcc2bb1086b9f306dff51
LouisDumont <33422071+LouisDumont@users.noreply.github.com>
2018-03-16 11:17:20 +0100

    Adding a first version of polak

--- src/monitor.py
+++ src/monitor.py
@@ -3,6 +3,7 @@
 import numpy as np
 from oracle import *
 from gradient import *
+from gradient_conjug_polak import *
 
 x0 = np.random.normal(size=n-md)
-gradient(oracle,x0)
+gradient_polak(oracle,x0)
