aa3b19a773cb84b36bc5aac0a6ef1f7f196eb2f0
Guillaume Desforges <aceus02@gmail.com>
2018-04-13 11:18:04 +0200

    Debut oracle lagrange

--- src/oracle.py
+++ src/oracle.py
@@ -14,7 +14,7 @@ def oracle(qc, compute_gradient=True, compute_hessian=False):
     loss = 1./3*dot(q, r*q*np.abs(q)) + dot(pr, dot(Ar, q))
     # Dérivée du critère par rapport à qc (à calculer)
     gradient = dot(t(B), r*q*abs(q)) + dot(dot(t(B), t(Ar)), pr) if compute_gradient else None
-    hessian = 2 * dot(t(B),t(r*abs(q)*t(B))) if compute_hessian else None
+    hessian = 2 * dot(t(B), t(r*abs(q)*t(B))) if compute_hessian else None
     
     return loss, gradient, hessian
 
---
+++ src/oracle_lagrange.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+from datas_r import *
+
+import numpy as np
+from numpy import dot
+from numpy import transpose as t
+
+# On écrit "u" dans le programme pour "lambda" dans le lagrangien
+# car lambda est un mot clé de python
+def q_hat(u):
+    a = - (dot(t(Ar), pr) + dot(t(Aq), u)) / r
+    s = np.sign(a)
+    return s*np.sqrt(np.abs(a))
+
+def oracle(u, compute_gradient=True, compute_hessian=False):
+    q = q_hat(u)
+    # Critère
+    loss = 1./3*dot(q, r*q*np.abs(q)) + dot(pr, dot(Ar, q)) + dot(t(u), dot(Ad, q) - fd)
+    # Dérivée du critère par rapport à u
+    gradient = dot(Ad, q) - fd if compute_gradient else None
+    hessian = 2 * dot(t(B), t(r*abs(q)*t(B))) if compute_hessian else None
+    
+    return loss, gradient, hessian
+
+if __name__ == '__main__':
+    # Test
+    qc = np.random.normal(size=n-md)
+    loss, gradient, hessian = oracle(qc,compute_hessian=True)
+    print(loss)
+    print(gradient)
+    print(hessian)
