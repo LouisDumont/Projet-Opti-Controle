Made Newon and fixed gradient work

src/oracle_lagrange.py
@@ -9,16 +9,19 @@ from numpy import transpose as t
 # On écrit "u" dans le programme pour "lambda" dans le lagrangien
 # car lambda est un mot clé de python
 def q_hat(u):
-    a = - (dot(t(Ar), pr) + dot(t(Aq), u)) / r
+    a = - (dot(t(Ar), pr) + dot(t(Ad), u)) / r
     s = np.sign(a)
-    return s*np.sqrt(np.abs(a))
+    delta = - np.diag(1/(r*np.sqrt(np.abs(a))))
+    return (s*np.sqrt(np.abs(a)), delta)
 
 def oracle(u, compute_gradient=True, compute_hessian=False):
-    q = q_hat(u)
+    q, delta = q_hat(u)
     # Critère
-    loss = 1./3*dot(q, r*q*np.abs(q)) + dot(pr, dot(Ar, q)) + dot(t(u), dot(Ad, q) - fd)
+    # on prend l'opposée du critère réel pour passer à un problème de minimisation
+    loss = -(1./3*dot(q, r*q*np.abs(q)) + dot(pr, dot(Ar, q)) + dot(t(u), dot(Ad, q) - fd))
     # Dérivée du critère par rapport à u
-    gradient = dot(Ad, q) - fd if compute_gradient else None
-    hessian = 2 * dot(t(B), t(r*abs(q)*t(B))) if compute_hessian else None
+    # On prend l'opposée du critère réel pour passer à un problème de minimisation
+    gradient = -( dot(Ad, q) - fd) if compute_gradient else None
+    hessian = -dot(dot(Ad, delta), t(Ad)) if compute_hessian else None
     
     return loss, gradient, hessian